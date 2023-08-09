62aa38105604eae98860aff3a147ff5fa371a39f
kiemaBruce <brucekiema292@gmail.com>
2023-08-09 22:47:43 +0300

    Reduce extra unnecessary funtion

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-# number = 3.14159
-number = 0
-if number == 0:
-    print("0.00")
-else:
-    print(f"Float: {round(number, 2)}")
+number = 3.14159
+print(f"Float: {round(number, 2)}")
