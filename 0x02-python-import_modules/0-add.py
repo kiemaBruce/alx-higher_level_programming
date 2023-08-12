106c4145181932ddee810312ab55203a0790af47
kiemaBruce <brucekiema292@gmail.com>
2023-08-12 12:56:47 +0300

    Use str.format() instead of fstrings

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
+
 if __name__ == "__main__":
     a = 1
     b = 2
-    sum = add(a, b)
-    print(f"{a:d} + {b:d} = {sum:d}")
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
