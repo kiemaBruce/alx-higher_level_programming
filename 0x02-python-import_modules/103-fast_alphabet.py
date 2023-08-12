625421c76e4866dd68fcba83dc8a576b628e95d9
kiemaBruce <brucekiema292@gmail.com>
2023-08-12 22:37:20 +0300

    Replace double quotes with single quotes

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 import requests, bs4, re
-
-print(re.sub('(<html>)|({})'.format("</html>\s*"), '', (requests.get("https://kiemabruce.github.io/uppercase-alphabet/")).text))
+print(re.sub('(<html>)|({})'.format('</html>\s*'), '', (requests.get('https://kiemabruce.github.io/uppercase-alphabet/')).text))
