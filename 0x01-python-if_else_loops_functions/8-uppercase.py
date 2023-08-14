ee31cb36c4053e39a7c161cb2cbfae3c10b4b34a
kiemaBruce <brucekiema292@gmail.com>
2023-08-14 22:39:41 +0300

    Remove extra print function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,8 @@ def uppercase(str):
     for i in range(len(str)):
         asc = ord(str[i])
         if asc > 96 and asc < 123:
-            print(chr(asc - 32), end="")
+            print("{:s}{:s}".format(chr(asc - 32), "\n"
+                  if i == (len(str) - 1) else ""), end="")
         else:
-            print(chr(asc), end="")
-    print()
+            print("{:s}{:s}".format(chr(asc), "\n"
+                  if i == (len(str) - 1) else ""), end="")
