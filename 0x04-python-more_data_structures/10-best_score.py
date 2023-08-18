6b36533e9e0f0389b3373eafa8c33e95ae50159c
kiemaBruce <brucekiema292@gmail.com>
2023-08-18 10:47:04 +0300

    Return None for an empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     counter = 0
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     for key in a_dictionary:
         value = a_dictionary.get(key)
