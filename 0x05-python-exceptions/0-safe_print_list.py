a625bf7f9f6e9dc7e8077281a559ee64b35b862e
kiemaBruce <brucekiema292@gmail.com>
2023-08-28 21:13:04 +0300

    Correct output for out-of-index values

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,13 @@ def safe_print_list(my_list=[], x=0):
     b = 0
 
     try:
-        for i in my_list:
+        while 1:
             if b == x:
                 break
-            print(i, end="" if i < x else "\n")
+            print(my_list[b], end="")
             b += 1
     except IndexError as r:
-        print(r.agrs[0])
+        print()  # newline only
+    else:
+        print()
     return b
