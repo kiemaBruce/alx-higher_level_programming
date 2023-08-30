bad238d2cd856101534a38ee23196fdb67be9191
kiemaBruce <brucekiema292@gmail.com>
2023-08-31 02:20:24 +0300

    Make code compliant with Pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -77,6 +77,9 @@ class Square:
                             "position must be a tuple of 2 positive integers"
                         )
                 else:
-                    raise TypeError("position must be a tuple of 2 positive integers")
+                    raise TypeError(
+                        "position must be a tuple of 2 positive\
+                            integers"
+                    )
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
