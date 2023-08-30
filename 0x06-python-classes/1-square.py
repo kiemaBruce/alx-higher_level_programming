f0ba13d9c61692664cddba7877cbe6d7ef39b276
kiemaBruce <brucekiema292@gmail.com>
2023-08-30 03:24:09 +0300

    Instantiate a private instance attribure for square object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains the Square class
+"""
+
+
+class Square:
+    """A class that defines a square
+
+    Attributes:
+            size: the size of the square
+    """
+
+    def __init__(self, size):
+        self.__size = size
