f21e58e4c7d9490d28e7e7c8ae99053e6ab69775
kiemaBruce <brucekiema292@gmail.com>
2023-09-05 23:13:30 +0300

    Add two integers and create tests for that

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains the add_integer function.
+>>> add_integer(2, 3)
+5
+"""
+
+
+def add_integer(a, b=98):
+    """ adds two integers and returns the result
+        Args:
+            a: first integer.
+            b: second integer; default value is 98.
+        Returns:
+            int: the arithmetic sum of a and b. The result is cast to an
+            integer.
+        Raises:
+            TypeError: if either a or b is not an integer or float.
+    """
+    if not isinstance(a, int) or isinstance(a, bool):
+        if not isinstance(a, float):
+            raise TypeError("a must be an integer")
+    if not isinstance(b, int) or isinstance(b, bool):
+        if not isinstance(b, float):
+            raise TypeError("b must be an integer")
+    return int(a) + int(b)
