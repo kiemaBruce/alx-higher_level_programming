6e63de9416039465c7e135e7d67d8a2c80679900
kiemaBruce <brucekiema292@gmail.com>
2023-09-08 04:11:35 +0300

    Correct rectangle and area methods

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -56,12 +56,12 @@ class Rectangle:
 
     def area(self):
         """Returns the rectangle area"""
-        if width == 0 or height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return self.__width * self.__height
 
     def perimeter(self):
         """Returns the rectangle area"""
-        if width == 0 or height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return self.__width * self.__height
+        return (2 * (self.__width + self.__height))
