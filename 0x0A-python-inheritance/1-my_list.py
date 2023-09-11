a33e6da4ee7697286f35429c86b3186ccdc02b01
kiemaBruce <brucekiema292@gmail.com>
2023-09-11 15:22:20 +0300

    Print a list in reverse order

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains the definition of MyList class that inherits from the list class.
+"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """Prints a list in reverse order."""
+        print(sorted(self, reverse=True))
