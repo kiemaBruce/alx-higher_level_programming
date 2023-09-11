f6b7ae043e9531a236011b5c0a5dc95e9569382a
kiemaBruce <brucekiema292@gmail.com>
2023-09-11 19:33:11 +0300

    Write class documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,9 @@ Contains the definition of MyList class that inherits from the list class.
 
 
 class MyList(list):
+    """
+    Inherits from 'list' class and creates a new class to print list in reverse
+    """
     def print_sorted(self):
         """Prints a list in reverse order."""
         print(sorted(self))
