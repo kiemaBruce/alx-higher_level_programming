40e4201bca775a713869962b6fef132b7e12d394
kiemaBruce <brucekiema292@gmail.com>
2023-09-12 00:18:22 +0300

    Remove repetitive code by reusing base class code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -36,26 +36,7 @@ class Rectangle(BaseGeometry):
                 width (int): width of the Rectangle.
                 height (int): height of the Rectangle.
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def area(self):
-        """Raises an exception."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates value
-        Args:
-               value (int): the value that is to be validated.
-               name (str): identifier of the value.
-        Raises:
-               TypeError: if value is not an integer.
-               ValueError: if value is not greater than 0 or if name is
-                           not a string or if name is not a string.
-        """
-        if not type(value) is int:
-            raise TypeError(f"{name:s} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name:s} must be greater than 0")
