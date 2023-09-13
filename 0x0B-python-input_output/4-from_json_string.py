af3debe266c366adee26463f435265be83d2cf27
kiemaBruce <brucekiema292@gmail.com>
2023-09-13 10:21:26 +0300

    Include function documentation

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,4 +7,10 @@ import json
 
 
 def from_json_string(my_str):
+    """Returns the Python object represented by a JSON string.
+    Args:
+         my_str (str): the JSON string.
+    Returns:
+        Python Object: the equivalent Python object of a JSON string.
+    """
     return json.loads(my_str)
