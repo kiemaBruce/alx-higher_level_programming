a455fcf6c8f51c1e40eb27bf91bd17c2d6baaed1
kiemaBruce <brucekiema292@gmail.com>
2023-09-13 10:23:03 +0300

    Document the module

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-import sys
-import json
-
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-
 """
 This module adds all arguments to it into a Python List and then saves them
 into a file. The file is saved in JSON format.
 """
+
+
+import sys
+import json
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 filename = "add_item.json"
