de7f9f65d0100f0deaae3bcb9f81969c278b3881
kiemaBruce <brucekiema292@gmail.com>
2023-09-15 11:32:32 +0300

    Create tests for task 1

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines the tests for the Base class.
+"""
+
+
+import unittest
+import os
+from models.base import Base
+
+os.chdir("../..")
+
+
+class TestBase(unittest.TestCase):
+    """
+    This classs performs unittests on the Base class.
+    """
+
+    def test_init(self):
+        """Tests whether the id is properly initialized.
+        """
+        base1 = Base(24)
+        self.assertEqual(base1.id, 24)
+        base2 = Base()
+        self.assertEqual(base2.id, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
