250615aba123394b7764156133ce929de47187fa
kiemaBruce <brucekiema292@gmail.com>
2023-09-17 22:35:26 +0300

    Update the update() method to now support **kwargs
    Object attributes can now be updated using the keyworded arguments but this only
    takes effect when *args doesn't exist and isn't empty.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,17 +110,31 @@ class Rectangle(Base):
         )
         return ret_s
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ Updates Rectangle attributes.
         """
-        for i in range(len(args)):
-            if i == 0:
-                self.id = args[0]
-            if i == 1:
-                self.width = args[1]
-            if i == 2:
-                self.height = args[2]
-            if i == 3:
-                self.x = args[3]
-            if i == 4:
-                self.y = args[4]
+        if args and len(args != 0):
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                if i == 2:
+                    self.height = args[2]
+                if i == 3:
+                    self.x = args[3]
+                if i == 4:
+                    self.y = args[4]
+        else:
+            if kwargs:
+                for key in kwargs:
+                    if key == "id":
+                        self.id = kwargs[key]
+                    if key == "width":
+                        self.width = kwargs[key]
+                    if key == "height":
+                        self.height = kwargs[key]
+                    if key == "x":
+                        self.x = kwargs[key]
+                    if key == "y":
+                        self.y = kwargs[key]
