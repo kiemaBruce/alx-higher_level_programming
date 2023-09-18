fde5ab05fff5f726f6da4205ed9d45c31623325f
kiemaBruce <brucekiema292@gmail.com>
2023-09-18 11:42:05 +0300

    Include tests for the __str__ method

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -6,6 +6,9 @@ This module defines the TestSquare class.
 
 import unittest
 from models.square import Square
+from unittest.mock import patch
+import sys
+import io
 
 
 class TestSquare(unittest.TestCase):
@@ -24,4 +27,18 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(
             str(con.exception),
             "__init__() missing 1 required positional argument: 'size'",
+        )
+
+    def test_str(self):
+        """Tests whether the __str__ method works as expected."""
+        s1 = Square(5)
+        with patch("sys.stdout", new_callable=io.StringIO) as mock_stdout:
+            print(s1)
+            captured_output = mock_stdout.getvalue().strip()
+        self.assertEqual(captured_output, "[Square] (1) 0/0 - 5")
+        with self.assertRaises(TypeError) as con:
+            Square.__str__()
+        self.assertEqual(
+            str(con.exception),
+            "__str__() missing 1 required positional argument: 'self'",
         )
