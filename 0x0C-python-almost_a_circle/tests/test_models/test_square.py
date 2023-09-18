365593103cb4e10f9818d90a03667e94c7c7cf90
kiemaBruce <brucekiema292@gmail.com>
2023-09-18 14:52:41 +0300

    Create tests for the update() method of Square class

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -57,3 +57,49 @@ class TestSquare(unittest.TestCase):
         with self.assertRaises(ValueError) as con2:
             s1.size = 0
         self.assertEqual(str(con2.exception), "width must be > 0")
+
+    def test_update(self):
+        """Tests the update method of Square class.
+        """
+        s1 = Square(5)
+        self.assertEqual((s1.id, s1.size), (4, 5))
+        s1.update()
+        self.assertEqual(
+            (s1.id, s1.size, s1.x, s1.y),
+            (4, 5, 0, 0)
+        )
+        s1.update(1)
+        self.assertEqual(s1.id, 1)
+        s1.update(1, 6)
+        self.assertEqual((s1.id, s1.size), (1, 6))
+        s1.update(1, 6, 7)
+        self.assertEqual((s1.id, s1.size, s1.x), (1, 6, 7))
+        s1.update(1, 6, 7, 4)
+        self.assertEqual(
+            (s1.id, s1.size, s1.x, s1.y), (1, 6, 7, 4)
+        )
+        with self.assertRaises(ValueError) as con:
+            s1.update(1, -3)
+        self.assertEqual(str(con.exception), "width must be > 0")
+        with self.assertRaises(ValueError) as con:
+            s1.update(2, 3, 4, -1)
+        self.assertEqual(str(con.exception), "y must be >= 0")
+        with self.assertRaises(TypeError) as con:
+            Square.update()
+        self.assertEqual(
+            str(con.exception),
+            "update() missing 1 required positional argument: 'self'",
+        )
+        s1.update(size=2)
+        self.assertEqual(s1.size, 2)
+        s1.update(size=21, x=11)
+        self.assertEqual((s1.size, s1.x), (21, 11))
+        s1.update(x=15, id=90, y=12)
+        self.assertEqual((s1.x, s1.id, s1.y), (15, 90, 12))
+        s1.update(x=15, id=90, y=12, size=87)
+        self.assertEqual(
+            (s1.x, s1.id, s1.y, s1.size),
+            (15, 90, 12, 87)
+        )
+        s1.update(1, 23, 12, size=44)
+        self.assertEqual(s1.size, 23)
