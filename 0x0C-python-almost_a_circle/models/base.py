d7aafae858350cb765ee4c7a6601c7203ff452df
kiemaBruce <brucekiema292@gmail.com>
2023-09-18 17:06:08 +0300

    Create list_dictionaries() static method in Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 Definition of the Base class.
 """
 
+import json
+
 
 class Base:
     """Defines the Base class."""
@@ -19,3 +21,12 @@ class Base:
         else:
             self.__class__.__nb_objects += 1
             self.id = self.__class__.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns the JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        json_s = json.dumps(list_dictionaries)
+        return json_s
