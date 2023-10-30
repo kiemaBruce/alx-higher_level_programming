75a262d9cb5c372cb0c4077c638451668f597e78
kiemaBruce <brucekiema292@gmail.com>
2023-10-30 23:59:46 +0300

    Correct wrong attribute name

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -56,6 +56,6 @@ class Rectangle:
     def perimeter(self):
         """Returns the Rectangle perimeter.
         """
-        if self.__width == 0 or self._height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
