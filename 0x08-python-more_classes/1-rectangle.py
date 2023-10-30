afde16077bd0dca053ac714f9c020e78cef2fb95
kiemaBruce <brucekiema292@gmail.com>
2023-10-30 23:38:42 +0300

    Create init method and getter and setter methods
    The setter and getter methods were created for the width and height.

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+    """Definition of the Rectangle class."""
+
+    def __init__(self, width=0, height=0):
+        """Initializes instance attributes.
+        Args:
+            width (int): width of the Rectangle object.
+            height (int): height of the Rectangle object.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Returns the width of the Rectangle object.
+        The setter raises a TypeError if width is not an int, and a ValueError
+        if width is less than 0.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height of the Rectangle object.
+        The setter raises a TypeError if height is not an int, and a ValueError
+        if it is less than 0.
+        """
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
