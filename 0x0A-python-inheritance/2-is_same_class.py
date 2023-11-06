bdcb9aea8b619ababaa53606029227c7c0f0e2e8
kiemaBruce <brucekiema292@gmail.com>
2023-11-06 21:49:24 +0300

    Refactor code

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -11,7 +11,7 @@ def is_same_class(obj, a_class):
     Return: True if obj is exactly an instance of class, and false
             otherwise.
     """
-    if isinstance(obj, a_class) and a_class.__name__ != "object":
+    if isinstance(obj, a_class):
         if (
             issubclass(obj.__class__, a_class)
             and obj.__class__.__name__ != a_class.__name__
