12321210b377417972c3a425a50e1d229912bd83
kiemaBruce <brucekiema292@gmail.com>
2023-11-07 18:28:37 +0300

    Return the Python object represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Contains the definition of the from_json_string() function.
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns the Python object represented by a JSON string"""
+    return json.loads(my_str)
