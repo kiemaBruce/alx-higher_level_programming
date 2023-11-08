226143486948fe89da1a2f21e0d72869c37b62f0
kiemaBruce <brucekiema292@gmail.com>
2023-11-08 15:15:05 +0300

    Return a Pascal's triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Contains the definition of pascal_triangle() function.
+"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists representing the Pascal's triangle of n.
+    Args:
+            n (int): number whose Pascal's triangle is to be determined.
+    """
+    ret_list = []
+    c = 0
+    for i in range(1, n + 1):
+        sub_list = []
+        for j in range(i):
+            if j == 0 or j == i - 1:
+                sub_list.append(1)
+            else:
+                sub_list.append((ret_list[c - 1])[j - 1]
+                                + (ret_list[c - 1])[j]
+                                )
+        ret_list.append(sub_list)
+        c += 1
+    return ret_list
