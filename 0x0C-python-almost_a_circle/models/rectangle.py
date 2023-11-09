39b8be58cd1c8e607715f5b7f8951be89e245ef9
kiemaBruce <brucekiema292@gmail.com>
2023-11-09 18:08:03 +0300

    Add validation for attributes
    width, height, x, and y attributes are validated. id is not validated.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,6 +31,10 @@ class Rectangle(base.Base):
 
     @width.setter
     def width(self, value):
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -40,6 +44,10 @@ class Rectangle(base.Base):
 
     @height.setter
     def height(self, value):
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -49,6 +57,10 @@ class Rectangle(base.Base):
 
     @x.setter
     def x(self, value):
+        if not type(value) is int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -58,4 +70,8 @@ class Rectangle(base.Base):
 
     @y.setter
     def y(self, value):
+        if not type(value) is int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
