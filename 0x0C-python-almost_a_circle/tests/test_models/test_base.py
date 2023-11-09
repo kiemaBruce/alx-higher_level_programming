1ee916b1c7addc6cea0a9d5e3129db750cc7c261
kiemaBruce <brucekiema292@gmail.com>
2023-11-09 11:28:55 +0300

    Create tests for base.py

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Contains TestBase which tests base.py.
+"""
+
+
+import unittest, os, sys
+
+
+os.chdir("models")
+current_dir = os.getcwd()
+sys.path.append(current_dir)
+Base = __import__('base').Base
+
+class TestBase(unittest.TestCase):
+	"""Contains the tests for Base class.
+	"""
+	def test_init(self):
+		"""Tests whether attributes are initialized properly.
+		"""
+		base1 = Base()
+		self.assertEqual(1, base1.id)
+		base1 = Base(12)
+		self.assertEqual(12, base1.id)
+		base1 = Base()
+		self.assertEqual(2, base1.id)
