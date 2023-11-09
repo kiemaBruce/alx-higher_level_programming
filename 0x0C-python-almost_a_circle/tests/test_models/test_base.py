573fe5c69753867da2ed92af6f05ff041fb79702
kiemaBruce <brucekiema292@gmail.com>
2023-11-09 13:58:18 +0300

    Format code to be compliant with Pycodestyle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,23 +3,26 @@
 """
 
 
-import unittest, os, sys
+import unittest
+import os
+import sys
 
 
 os.chdir("models")
 current_dir = os.getcwd()
 sys.path.append(current_dir)
-Base = __import__('base').Base
+
+Base = __import__("base").Base
+
 
 class TestBase(unittest.TestCase):
-	"""Contains the tests for Base class.
-	"""
-	def test_init(self):
-		"""Tests whether attributes are initialized properly.
-		"""
-		base1 = Base()
-		self.assertEqual(1, base1.id)
-		base1 = Base(12)
-		self.assertEqual(12, base1.id)
-		base1 = Base()
-		self.assertEqual(2, base1.id)
+    """Contains the tests for Base class."""
+
+    def test_init(self):
+        """Tests whether attributes are initialized properly."""
+        base1 = Base()
+        self.assertEqual(1, base1.id)
+        base1 = Base(12)
+        self.assertEqual(12, base1.id)
+        base1 = Base()
+        self.assertEqual(2, base1.id)
