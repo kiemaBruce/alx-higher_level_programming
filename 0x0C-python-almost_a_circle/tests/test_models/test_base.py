f93b3bd67d4b8e1eadb41e97c5d17179faac3de3
kiemaBruce <brucekiema292@gmail.com>
2023-11-13 15:39:37 +0300

    Create tests for to_json_string()

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -26,3 +26,19 @@ class TestBase(unittest.TestCase):
         self.assertEqual(12, base1.id)
         base1 = Base()
         self.assertEqual(2, base1.id)
+
+    def test_list_dictionaries(self):
+        """Tests to_json_string()"""
+        t_dict = dict(width=2, x=7, height=22, y=77, info=True)
+        json_s = '{"width": 2, "x": 7, "height": 22, "y": 77, "info": true}'
+        self.assertEqual(Base.to_json_string(t_dict), json_s)
+        self.assertEqual(Base.to_json_string(None), "[]")
+        empty_list = []
+        self.assertEqual(Base.to_json_string(empty_list), "[]")
+        with self.assertRaises(TypeError) as c:
+            Base.to_json_string()
+        self.assertEqual(
+            str(c.exception),
+            "to_json_string() missing 1 required positional argument: "
+            "'list_dictionaries'"
+        )
