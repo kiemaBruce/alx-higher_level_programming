e509e406c6b853e65d23800622370bfc1918b131
kiemaBruce <brucekiema292@gmail.com>
2023-11-13 22:11:23 +0300

    Create to_json_string() method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,27 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file.
+        Args:
+            list_objs (list): a list of instances who inherits of Base -
+                              example: list of Rectangle or list of
+                              Square instances. If list_objs is None, an empty
+                              list is saved. The filename is <Class name>.json
+                              for example: Rectangle.json
+        """
+        if list_objs is None:
+            filename = list_objs.__class__.__name__ + ".json"
+            with open(filename, mode="w", encoding="utf-8") as myfile:
+                myfile.write('[]')
+        else:
+            dict_list = []
+            if len(list_objs) != 0:
+                for item in list_objs:
+                    dict_list.append(item.to_dictionary())
+                dict_list_json = cls.to_json_string(dict_list)
+                filename = list_objs[0].__class__.__name__ + ".json"
+                with open(filename, mode="w", encoding="utf-8") as myfile:
+                    myfile.write(dict_list_json)
