4c176d86115c900780172af6c3096f0387351bfe
kiemaBruce <brucekiema292@gmail.com>
2024-02-22 15:37:01 +0300

    Un-indent code

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,6 @@
 SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
-    ON tv_genres.id = tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.show_id = 8
 ORDER BY tv_genres.name ASC;
