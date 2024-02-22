8559ae872592a91ea09afdba1c0862d3ea1a4f11
kiemaBruce <brucekiema292@gmail.com>
2024-02-22 22:00:57 +0300

    List all Comedy shows in database

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Lists all comedy shows in the database.
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id = 5
+ORDER BY tv_shows.title ASC;
