debfbd706c2a779c454ee30ce38fc7fe5886b184
kiemaBruce <brucekiema292@gmail.com>
2024-02-22 06:38:30 +0300

    Only create table if it doesn't exist

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Create table with unique data type that also has a default value
-CREATE TABLE unique_id(
+CREATE TABLE IF NOT EXISTS unique_id(
     id   INT          UNIQUE DEFAULT 1,
     name VARCHAR(256)
 );
