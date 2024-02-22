763ceab0b56529a10476985fe10e1480513fdb69
kiemaBruce <brucekiema292@gmail.com>
2024-02-22 22:28:10 +0300

    Check for Dexter tv show using title and not id

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,10 @@
 -- List all genres of the show 'Dexter'
 SELECT tv_genres.name
-    FROM tv_genres
+    FROM tv_shows
         INNER JOIN tv_show_genres
-        ON tv_genres.id = tv_show_genres.genre_id
-    WHERE tv_show_genres.show_id = 8
+        ON tv_shows.id = tv_show_genres.show_id
+
+        INNER JOIN tv_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_shows.title = 'Dexter'
     ORDER BY tv_genres.name ASC;
