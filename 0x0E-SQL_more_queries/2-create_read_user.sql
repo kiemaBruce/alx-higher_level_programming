6fa12d0122904d2dbddb8b41d3808418e514d475
kiemaBruce <brucekiema292@gmail.com>
2024-02-22 06:07:30 +0300

    Grant SELECT privilege to hbtn_0d_2 database only

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create user.
 CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
 -- Grant SELECT privileges.
-GRANT SELECT ON *.* TO 'user_0d_2';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2';
