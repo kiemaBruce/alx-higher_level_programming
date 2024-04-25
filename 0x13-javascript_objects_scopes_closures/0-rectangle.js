acc853bcdc250fda8b049466edbd97137710a6d7
kiemaBruce <brucekiema292@gmail.com>
2024-04-25 05:28:41 +0300

    Add comment

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor () {
-    //
+    /**
+     * This constructor is empty because this is an empty class definition.
+     */
   }
 }
 module.exports = Rectangle;
