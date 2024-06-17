eac6fdab83c1b63dcd97a89e6da87ace67ea03a3
kiemaBruce <brucekiema292@gmail.com>
2024-06-17 23:49:07 +0300

    Print an array of strings using a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let position = 0; position < 3; position++) {
+  console.log(myStrings[position]);
+}
