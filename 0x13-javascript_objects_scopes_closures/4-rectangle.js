3519f2be6ce11de8ebfbee29b3dbdcedcc79e232
kiemaBruce <brucekiema292@gmail.com>
2024-06-18 23:43:20 +0300

    Include rotate and double instance methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myStr = '';
+      for (let j = 0; j < this.width; j++) {
+        myStr += 'X';
+      }
+      console.log(myStr);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
