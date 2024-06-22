3f0de46a75a55a7b2b99deb871986e82ad1d16c1
kiemaBruce <brucekiema292@gmail.com>
2024-06-22 18:54:31 +0300

    Only list states that start with 'N' from hbtn_0e_0_usa database

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Lists all states from hbtn_0e_0_usa database"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    mySQL_username = sys.argv[1]
+    mySQL_password = sys.argv[2]
+    mySQL_database = sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", user=mySQL_username,
+                           passwd=mySQL_password, db=mySQL_database, port=3306)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' order by id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
