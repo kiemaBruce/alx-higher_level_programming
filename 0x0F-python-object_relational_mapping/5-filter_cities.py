745bf347e87c75b08c27f74df502d8979cf1f0c0
kiemaBruce <brucekiema292@gmail.com>
2024-06-23 20:22:09 +0300

    Format output to match requirements

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,15 +16,19 @@ if __name__ == "__main__":
                                passwd=mySQL_password, db=mySQL_database,
                                port=3306)
         cur = conn.cursor()
-        query = """SELECT cities.id, cities.name, states.name
+        query = """SELECT cities.name
         FROM cities INNER JOIN states
         ON states.id = cities.state_id
         WHERE states.name = %s
         ORDER BY cities.id ASC"""
         cur.execute(query, (state,))
         cities = cur.fetchall()
+        cities_arr = []
         for city in cities:
-            print(city)
+            cities_arr.append(city[0])
+        separator = ", "
+        cities_str = separator.join(cities_arr)
+        print(cities_str)
     except MySQLdb.Error as e:
         print(e)
     finally:
