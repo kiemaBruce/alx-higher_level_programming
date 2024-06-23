9b3fd1afda8fd7a0f1de6e6fd7ee76ad30d64f60
kiemaBruce <brucekiema292@gmail.com>
2024-06-23 14:29:45 +0300

    Add try except blocks to catch errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,13 +5,21 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    mySQL_username = sys.argv[1]
-    mySQL_password = sys.argv[2]
-    mySQL_database = sys.argv[3]
-    conn = MySQLdb.connect(host="localhost", user=mySQL_username,
-                           passwd=mySQL_password, db=mySQL_database, port=3306)
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states order by id ASC")
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+    conn = None
+    try:
+        mySQL_username = sys.argv[1]
+        mySQL_password = sys.argv[2]
+        mySQL_database = sys.argv[3]
+        conn = MySQLdb.connect(host="localhost", user=mySQL_username,
+                               passwd=mySQL_password, db=mySQL_database,
+                               port=3306)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states order by id ASC")
+        states = cur.fetchall()
+        for state in states:
+            print(state)
+    except MySQLdb.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
