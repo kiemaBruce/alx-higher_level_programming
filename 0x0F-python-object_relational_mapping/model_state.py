6d3b90ed5c4102da81c6c9fcda4c42b0a1d80fb0
kiemaBruce <brucekiema292@gmail.com>
2024-06-25 09:58:11 +0300

    Modify code that's executed on import

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,12 +5,29 @@ This script connects to a MySQL database and creates the `states` table if it
 does not exist."""
 
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Definition of the State which defines of the states table
+
+        Attributes:
+            __tablename__ (str): the name of the table that this class defines.
+            id (int): primary key of the table which auto-increments.
+            name (str): the name of the state.
+
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+
 if __name__ == "__main__":
-    from sqlalchemy import Column, Integer, String
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import declarative_base
-    """from sqlalchemy.ext.declarative import declarative_base"""
-    from sqlalchemy.orm import sessionmaker
     import sys
 
     user = sys.argv[1]
@@ -19,26 +36,10 @@ if __name__ == "__main__":
     port = 3306
     database = sys.argv[3]
     engine = create_engine(
-                (
-                    f'mysql+mysqlconnector://{user}:{password}@{host}'
-                    f':{port}/{database}'
+            (
+                f'mysql+mysqlconnector://{user}:{password}@{host}'
+                f':{port}/{database}'
                 ),
-                pool_pre_ping=True
+            pool_pre_ping=True
             )
-    Base = declarative_base()
-
-    class State(Base):
-        """
-        Definition of the State which defines of the states table
-
-        Attributes:
-            __tablename__ (str): the name of the table that this class defines.
-            id (int): primary key of the table which auto-increments.
-            name (str): the name of the state.
-
-        """
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True)
-        name = Column(String(128), nullable=False)
-
     Base.metadata.create_all(engine)
