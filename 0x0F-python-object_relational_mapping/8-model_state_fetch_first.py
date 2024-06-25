04f6f6d1227f5c0bdfc74e6d1ba51ec3eedccc75
kiemaBruce <brucekiema292@gmail.com>
2024-06-25 11:28:37 +0300

    Print a string if no records are found

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -27,4 +27,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     with Session() as session:
         first_state = session.query(State).first()
-        print(f'{first_state.id}: {first_state.name}')
+        if first_state is None:
+            print('Nothing')
+        else:
+            print(f'{first_state.id}: {first_state.name}')
