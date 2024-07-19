5eb32448a9c01f22cd6132d4b59b0f6a7aa6c9a8
kiemaBruce <brucekiema292@gmail.com>
2024-07-19 23:32:48 +0300

    Modify shebang line to match project requirements

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Displays the body of http GET request only if it has a status code of 200
 if [[ $(curl -sI "$1" | grep "HTTP/" | cut -d ' ' -f 2) -eq 200 ]]; then curl "$1"; fi
