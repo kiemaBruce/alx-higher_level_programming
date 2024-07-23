fb80bc7d20cd46a99af86a3b7e72baf5f8493737
kiemaBruce <brucekiema292@gmail.com>
2024-07-23 17:12:35 +0300

    Fetch a url using urllib and display the body in a special format

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status using urllib"""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") \
+            as response:
+        the_page = response.read()
+    body_str = the_page.decode('utf-8')
+    body_type = type(the_page)
+    print("Body response:")
+    print(f"    - type: {body_type}")
+    print(f"    - content: {the_page}")
+    print(f"    - utf8 content: {body_str}")
