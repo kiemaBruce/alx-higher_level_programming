65b925c0eece807fe3ce7f75c808f98562a301e1
kiemaBruce <brucekiema292@gmail.com>
2024-07-23 17:31:48 +0300

    Perform everything within the urllib context manager

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") \
             as response:
         the_page = response.read()
-    body_str = the_page.decode('utf-8')
-    body_type = type(the_page)
-    print("Body response:")
-    print(f" - type: {body_type}")
-    print(f" - content: {the_page}")
-    print(f" - utf8 content: {body_str}")
+        body_str = the_page.decode('utf-8')
+        body_type = type(the_page)
+        print("Body response:")
+        print(f"\t- type: {body_type}")
+        print(f"\t- content: {the_page}")
+        print(f"\t- utf8 content: {body_str}")
