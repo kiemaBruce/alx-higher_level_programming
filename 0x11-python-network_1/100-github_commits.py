bcd4ffb5b5e3682262d7a2c51900202859655ca1
kiemaBruce <brucekiema292@gmail.com>
2024-07-24 14:01:42 +0300

    Show most recent 10 commits to a repo by a user

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Shows commits into a repository by a specific user"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    r = requests.get(url)
+    if r.status_code == 200:
+        commits_list = r.json()
+        formatted_commits = []
+        i = 0
+        for commit in commits_list:
+            if i == 10:
+                break
+            commit_str = ""
+            commit_str += commit.get('sha')
+            commit_str += ': '
+            commit_str += commit.get('commit').get('committer').get('name')
+            formatted_commits.append(commit_str)
+            i += 1
+        for item in formatted_commits:
+            print(item)
+    else:
+        print(None)
