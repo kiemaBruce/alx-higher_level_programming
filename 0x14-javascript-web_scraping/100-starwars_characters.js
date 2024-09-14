a3bf2a91360e09d55d611bb2478003e53e116184
kiemaBruce <brucekiema292@gmail.com>
2024-09-14 22:32:40 +0300

    List all characters in a Star Wars movie in order they appear in /films

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,6 +2,8 @@
 const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+let completedRequests = 0;
+const charactersObj = {};
 request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const filmObj = JSON.parse(body);
@@ -9,9 +11,17 @@ request(url, (error, response, body) => {
     for (const characterUrl of characters) {
       request(characterUrl, (error, response, body) => {
         if (!error && response.statusCode === 200) {
-          const characterObj = JSON.parse(body);
-          const characterName = characterObj.name;
-          console.log(characterName);
+          const index = parseInt(characterUrl.split('https://swapi-api.alx-tools.com/api/people/')[1].slice(0, -1));
+          const characterName = JSON.parse(body).name;
+          if (characterName) {
+            charactersObj[index] = characterName;
+          }
+          completedRequests++;
+          if (completedRequests === characters.length) {
+            for (const character in charactersObj) {
+              console.log(charactersObj[character]);
+            }
+          }
         } else if (error) {
           console.error(error);
         } else if (response.statusCode !== 200) {
